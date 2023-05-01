af1ee710d278c0c858b3ac8603f2742d1abe8278
Muhammad Ehtasham Ul haq <ahtashamshani1@gmail.com>
2023-05-01 22:08:31 +0500

    Write the mirgrations for the cinema systems

--- express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
+++ express_sequelize/src/database/migrations/1663877813247-CinemaSystem.ts
@@ -1,38 +1,160 @@
-import { QueryInterface } from 'sequelize';
+import { literal, QueryInterface } from 'sequelize';
+import { ModelAttributes } from 'sequelize/types/model';
 
 export default {
-  /**
-   # ToDo: Create a migration that creates all tables for the following user stories
+  up: async (queryInterface: QueryInterface): Promise<void> => {
+    await queryInterface.createTable('movietimes', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      movie: { type: 'varchar', allowNull: false },
+      url: { type: 'timestamp', allowNull: false },
+      isBookedOut: {
+        type: 'boolean',
+        defaultValue: false
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
 
-   For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
-   To not introduce additional complexity, please consider only one cinema.
+    await queryInterface.createTable('showrooms', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: 'varhcar',
+        allowNull: false
+      },
+      createdAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: 'timestamp',
+        defaultValue: literal('CURRENT_TIMESTAMP'),
+      },
+    } as ModelAttributes);
 
-   Please list the tables that you would create including keys, foreign keys and attributes that are required by the user stories.
+    await queryInterface.createTable('administrations', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: 'varhcar',
+        allowNull: false
+      },
+      createdAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      }
 
-   ## User Stories
+    } as ModelAttributes);
 
-   **Movie exploration**
-   * As a user I want to see which films can be watched and at what times
-   * As a user I want to only see the shows which are not booked out
+    await queryInterface.createTable('pricing', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      showtime_id: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: 'movietimes',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      price: {
+        type: 'decimal',
+        allowNull: false
+      },
+      premium_percentage: {
+        type: 'decimal',
+        allowNull: false
+      },
+      createdAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      }
 
-   **Show administration**
-   * As a cinema owner I want to run different films at different times
-   * As a cinema owner I want to run multiple films at the same time in different showrooms
+    } as ModelAttributes);
 
-   **Pricing**
-   * As a cinema owner I want to get paid differently per show
-   * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
+    await queryInterface.createTable('pricing', {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      showroom_id: {
+        type: 'integer',
+        allowNull: false,
+        references: {
+          model: 'showrooms',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      row: {
+        type: 'integer',
+        allowNull: false
+      },
+      number: {
+        type: 'integer',
+        allowNull: false
+      },
+      type: {
+        type: 'varchar',
+        allowNull: false
+      },
+      is_booked: {
+        type: 'boolean',
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        type: 'timestamp',
+        allowNull: false,
+        defaultValue: literal('CURRENT_TIMESTAMP')
+      }
 
-   **Seating**
-   * As a user I want to book a seat
-   * As a user I want to book a vip seat/couple seat/super vip/whatever
-   * As a user I want to see which seats are still available
-   * As a user I want to know where I'm sitting on my ticket
-   * As a cinema owner I don't want to configure the seating for every show
-   */
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  up: (queryInterface: QueryInterface): Promise<void> => {
-    throw new Error('TODO: implement migration in task 4');
+    } as ModelAttributes);
   },
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
